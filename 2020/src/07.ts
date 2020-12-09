f6ac736d2852ec5b35b60b36b5f0d368ad7a350c
Will Smith <will.smith@simplisafe.com>
2020-12-09 15:07:10 -0500

    day 7 part 2

--- 2020/src/07.ts
+++ 2020/src/07.ts
@@ -1,4 +1,5 @@
 // Day 7: Handy Haversacks
+import { count } from 'console';
 import { readStringList } from './lib/utils';
 
 function parseRules(ruleStrings: Array<string>): object {
@@ -43,7 +44,7 @@ function canContainColor(outsideColor: string, insideColor: string, rules: objec
   return false;
 }
 
-function countContainingColors(insideColor: string, rules: object) {
+function countContainingColors(insideColor: string, rules: object): number {
   let count = 0;
 
   for (let color in rules) {
@@ -55,6 +56,23 @@ function countContainingColors(insideColor: string, rules: object) {
   return count;
 }
 
+function countTotalBagsContained(outsideColor: string, rules: object): number {
+  const colorsObj = rules[outsideColor];
+  const colorsContained = Object.keys(colorsObj);
+
+  if (colorsContained.length === 0) {
+    return 0;
+  }
+
+  let sum = 0;
+  for (let color of colorsContained) {
+    const numBags = colorsObj[color];
+    sum += numBags + numBags*countTotalBagsContained(color, rules);
+  }
+
+  return sum;
+}
+
 
 
 // main
@@ -62,5 +80,5 @@ const inputFile = '../inputs/07.txt';
 const rulesRaw = readStringList(inputFile);
 const rules = parseRules(rulesRaw);
 
-// console.log(rules);
 console.log(`7-1 answer: ${countContainingColors('shiny gold', rules)}`);
+console.log(`7-2 answer: ${countTotalBagsContained('shiny gold', rules)}`);
